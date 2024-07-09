f239f09b6c3d464224f442d36dc6caef284f3aa9
timadewale1 <timadewale1@gmail.com>
2024-07-09 23:21:27 +0100

    commit

---
+++ src/config.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const config = () => {
+  apiKey: "AIzaSyC7pOCYSGpYMUDiRxRN4nV4UUfd2tdx1Jg",
+  authDomain: "ecommerce-ba520.firebaseapp.com",
+  projectId: "ecommerce-ba520",
+  storageBucket: "ecommerce-ba520.appspot.com",
+  messagingSenderId: "620187458799",
+  appId: "1:620187458799:web:c4deef3184a5145256cf1a",
+}
+
+export default config

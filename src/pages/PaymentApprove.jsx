efe754499daeccd99f9d69a98e769a279251ecd0
unrealtobi <tobiwunmie@gmail.com>
2024-11-02 13:40:43 +0100

    handled orders!

--- src/pages/PaymentApprove.jsx
+++ src/pages/PaymentApprove.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import Paymentsuccess from '../components/Loading/PaymentSuccess'
+import React, { useEffect } from 'react';
+import Paymentsuccess from '../components/Loading/PaymentSuccess';
+import chachingSound from '../Audio/cha-ching-sound-effect-download-128-ytshorts.savetube.me.mp3';
+
 const PaymentApprove = () => {
+  useEffect(() => {
+    // Play sound
+    const audio = new Audio(chachingSound);
+    audio.play().catch((error) => console.log("Error playing audio:", error));
+
+    // Trigger haptic feedback if supported
+    if (navigator.vibrate) {
+      navigator.vibrate([200, 100, 200]); // Vibration pattern
+    }
+  }, []);
+
   return (
     <div className='flex justify-center translate-y-48 w-full h-full'>
       <Paymentsuccess />
     </div>
-  )
-}
+  );
+};
 
-export default PaymentApprove
+export default PaymentApprove;

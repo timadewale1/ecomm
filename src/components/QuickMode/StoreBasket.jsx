6c2d6ce8f4f2178dac0648eb8f7d8cbc7586735d
unrealtobi <tobiwunmie@gmail.com>
2025-08-03 10:37:07 +0100

    finished

--- src/components/QuickMode/StoreBasket.jsx
+++ src/components/QuickMode/StoreBasket.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { removeFromCart } from "../../redux/actions/action";
 import { BsBasket } from "react-icons/bs";

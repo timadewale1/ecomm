6c93b796553ab3020f7df0bfff7a3bf239b08702
unrealtobi <tobiwunmie@gmail.com>
2025-08-12 12:23:19 +0100

    finished touches

--- src/pages/StorePage.jsx
+++ src/pages/StorePage.jsx
@@ -463,12 +463,12 @@ function NetworkIssueNotice({ onRetry }) {
           e.currentTarget.style.display = "none";
         }}
       />
-      <h1 className="text-xl font-bold font-opensans text-gray-800">
+      <h1 className="text-xl font-bold mt-24 font-opensans text-gray-800">
         Can’t reach My Thrift right now
       </h1>
       <p className="text-sm mt-2 text-gray-600 font-opensans">
         It looks like you’re offline or the connection is unstable. Please check
-        your internet and try again.
+        your internet and try again Or this store isn't accessible right now, check back later.
       </p>
       <button
         className="mt-5 py-2 px-5 rounded-full font-medium font-opensans bg-customOrange text-white"
